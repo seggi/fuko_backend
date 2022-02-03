320be5366abb5d1253b9df93486482c528190e0a
nankim <seggimarugira@gmail.com>
2022-02-03 05:47:29 +0200

    some changes

--- services/app/manage.py
+++ services/app/manage.py
@@ -38,7 +38,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello this is the new version!"
+    return "Hello this is the new versio!"
 
 if __name__ == "__main__":
     app.run()
