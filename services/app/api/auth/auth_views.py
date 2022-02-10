1e4414408f7f11afcc3e84d9eb7564091311c355
nankim <seggimarugira@gmail.com>
2022-02-10 14:31:43 +0200

    some changes

--- services/app/api/auth/auth_views.py
+++ services/app/api/auth/auth_views.py
@@ -100,13 +100,15 @@ def login_user():
             return response_with(resp.BAD_REQUEST_400)
         if User.verify_hash(data['password'], current_user.password):
             user = User.query.filter_by(email=data['email']).first()
-            access_token = create_access_token(identity={
-                "username": user.username,
-                "status": user.status,
-                "user_id": user.id
-            })
+            access_token = create_access_token(identity=data["email"])
             return response_with(resp.SUCCESS_201, value={'message': f'{current_user.username}',
-                                                          "access_token": access_token})
+                                                          "access_token": access_token,
+                                                          "data": {
+                                                              "username": user.username,
+                                                              "status": user.status,
+                                                              "user_id": user.id
+                                                          }
+                                                          })
         else:
             return response_with(resp.UNAUTHORIZED_403)
 
