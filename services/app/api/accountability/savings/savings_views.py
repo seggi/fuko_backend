1c05c39ae9a120d2a4c633871e13f5035b0fd508
nankim <seggimarugira@gmail.com>
2022-02-18 03:00:43 +0200

    savings

--- services/app/api/accountability/savings/savings_views.py
+++ services/app/api/accountability/savings/savings_views.py
@@ -5,37 +5,37 @@ from api.accountability.global_amount.global_amount_views import QUERY
 from api.core.query import QueryGlobalRepport
 from api.utils.responses import response_with
 from api.utils import responses as resp
-from api.utils.model_marsh import ExpensesSchema
+from api.utils.model_marsh import SavingsSchema
 
 from ... import db
-from api.database.models import Expenses, User
-expenses = Blueprint("expenses", __name__,
-                     url_prefix="/api/user/account")
+from api.database.models import Savings, User
+savings = Blueprint("savings", __name__,
+                    url_prefix="/api/user/account/savings")
 
 QUERY = QueryGlobalRepport()
 
 
-@expenses.post("/add-expenses/<int:user_id>")
+@savings.post("/add-saving/<int:user_id>")
 @jwt_required()
-def user_add_expenses(user_id):
+def user_add_saving(user_id):
     # Generate inputs
     data = request.json | {"user_id": user_id}
     if data["amount"] is None or data["description"] is None:
         return response_with(resp.INVALID_INPUT_422)
     else:
-        QUERY.insert_data(db=db, table_data=Expenses(**data))
+        QUERY.insert_data(db=db, table_data=Savings(**data))
     return jsonify({
         "code": "success",
         "message": "Amount saved with success"
     })
 
 
-@expenses.get("/expenses/<int:user_id>")
+@savings.get("/savings/<int:user_id>")
 @jwt_required()
-def user_get_expenses(user_id):
+def user_get_saving(user_id):
     item_list = []
-    expenses_schema = ExpensesSchema()
-    data = QUERY.get_data(db=db, model=Expenses, user_id=user_id)
+    savings_schema = SavingsSchema()
+    data = QUERY.get_data(db=db, model=Savings, user_id=user_id)
     for item in data:
-        item_list.append(expenses_schema.dump(item))
+        item_list.append(savings_schema.dump(item))
     return jsonify(data=item_list)
