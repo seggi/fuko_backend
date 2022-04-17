437c72e8a51b3ae4d6181fe7532ba59ca4995798
seggi <seggimarugira@gmail.com>
2022-04-17 19:45:56 +0200

    some change

--- services/app/api/utils/documents.py
+++ services/app/api/utils/documents.py
@@ -21,9 +21,67 @@ doc: dict = {
                                     'amount': 'float'
                                 }
                             '''
+                        },
+                        {
+                            'method': 'post',
+                            'end_point': '/save-budget-details/<int:budget_id>',
+                            'description': '',
+                            'inputs': '''
+                                {
+                                    'title': 'string',
+                                    'amount': 'float',
+                                    'description': 'string'
+                                }
+                            '''
+                        },
+                        {
+                            'method': 'get',
+                            'end_point': '/get-budget-details/<int:budget_id>',
+                            'description': '',
                         }
                     ],
                     
+                },
+
+                'dept': {
+                    'url_prefix': '/api/user/account/dept',
+                    'end_points': [
+                        {
+                            'method': 'post',
+                            'end_point': '/add-borrower-to-notebook',
+                            'description': '',
+                            'inputs': '''
+                                { 'name': 'string' }
+                            '''
+                        },
+                        {
+                            'method': 'post',
+                            'end_point': '/search-user',
+                            'description': '',
+                            'inputs': '''
+                                { 'name': 'string' }
+                            '''
+                        },
+                        {
+                            'method': 'get',
+                            'end_point': '/retrieve',
+                            'description': '',
+                        },
+                         {
+                            'method': 'post',
+                            'end_point': '/add-dept/<int:note_id>',
+                            'description': '',
+                            'inputs': '''
+                                { 'amount': float, description: 'string' }
+                            '''
+                        },
+                         {
+                            'method': 'get',
+                            'end_point': '/retrieve-date/<int:dept_note_id>',
+                            'description': 'Retrieve current month',
+                            
+                        },
+                    ]
                 }
             }
         }
