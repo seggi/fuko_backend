c22552f93e72415939cb1efb6521d2ea47548701
seggi <seggimarugira@gmail.com>
2022-04-19 17:22:49 +0200

    some add in documents.py

--- api/utils/documents.py
+++ api/utils/documents.py
@@ -43,6 +43,36 @@ doc: dict = {
                     
                 },
 
+                'loan':  {
+                  'url_prefix': '/api/user/account/loans',
+                  'end_points': [
+                      {
+                          'method': 'post',
+                          'end_point': '/add-partner-to-notebook',
+                          'description':'Add new financial partner',
+                          'inputs': '''
+                            { 'partner_name': string, 'partner_id': integer,  }
+                          '''
+                      },
+                      {
+                          'method': 'get',
+                          'end_point': '/retrieve',
+                          'description':'Get loans'
+                      },
+                      {
+                          'method': 'post',
+                          'end_point': '/search-user',
+                          'description':'Search for partner in the system',
+                          'inputs': '''{ username: string }'''
+                      },
+                      {
+                          'method': 'get',
+                          'end_point': '/retrieve-date/<int:loan_note_id>',
+                          'description':'Retrieve all loans in current month'
+                      }
+                  ]
+                },
+
                 'dept': {
                     'url_prefix': '/api/user/account/dept',
                     'end_points': [
@@ -78,7 +108,7 @@ doc: dict = {
                          {
                             'method': 'get',
                             'end_point': '/retrieve-date/<int:dept_note_id>',
-                            'description': 'Retrieve current month',
+                            'description': 'Retrieve all depts in current month',
                             
                         },
                     ]
