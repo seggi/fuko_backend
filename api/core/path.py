414096f5ab64c9b6a83db444fd1ad983b05314c4
Seggi SMS <seggimarugira@gmail.com>
2022-09-12 12:23:28 +0200

    some change

--- api/core/path.py
+++ api/core/path.py
@@ -4,8 +4,10 @@ import errno
 
 def create_path(directory):
     try:
-        os.makedirs(directory)
-        return 200
+        media_folder = os.path.normpath(directory)
+        if not os.path.exists(media_folder):
+            os.makedirs(media_folder)
+            return 200
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
